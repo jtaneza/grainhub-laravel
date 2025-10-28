438dc4c0884b634846295fb288e3b93fa0ed8725
jtaneza <j.taneza.546922@umindanao.edu.ph>
2025-10-28 22:50:37 +0800

    add SalesExport

---
+++ app/Exports/SalesExport.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Exports;
+
+use Illuminate\Contracts\View\View;
+use Maatwebsite\Excel\Concerns\FromView;
+
+class SalesExport implements FromView
+{
+    protected $sales;
+
+    public function __construct($sales)
+    {
+        $this->sales = $sales;
+    }
+
+    public function view(): View
+    {
+        return view('exports.sales', ['sales' => $this->sales]);
+    }
+}
