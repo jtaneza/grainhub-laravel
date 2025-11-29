f8a53ce9c3c6cec9bec0d4ae420ffc4f89ea0bb6
jtaneza <j.taneza.546922@umindanao.edu.ph>
2025-11-29 16:18:39 +0800

    update Group.php

---
+++ app/Models/Group.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+
+class Group extends Model
+{
+    protected $table = 'user_groups';
+    protected $fillable = ['group_name', 'group_level', 'group_status'];
+    public $timestamps = false;
+
+    public function users()
+    {
+        return $this->hasMany(User::class, 'user_level', 'group_level');
+    }
+}
